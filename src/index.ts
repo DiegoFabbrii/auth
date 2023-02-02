cca62df480e02a724ecd8561e7961197ffbb2689
DiegoFabbrii <diegofabbri@outlook.com>
2023-02-02 17:31:02 -0300

    feat: add server express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import express from 'express';
+import { userRoute } from './routes/user.route';
+const app = express();
 
+const port = 3004;
+
+app.use(express.json());
+app.use(userRoute);
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
