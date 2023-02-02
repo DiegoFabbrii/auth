929fd7899ae4469ca7965693aca00eb0030bea23
DiegoFabbrii <diegofabbri@outlook.com>
2023-02-02 17:43:44 -0300

    feat: add mongoose connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 import { userRoute } from './routes/user.route';
+
 const app = express();
+dotenv.config();
 
 const port = 3004;
 
-app.use(express.json());
-app.use(userRoute);
+mongoose.set('strictQuery', false);
+mongoose.connect(`${process.env.DB_CONNECTION}`).then(() => {
+  app.use(express.json());
+  app.use(userRoute);
 
-app.listen(port, () => console.log(`http://localhost:${port}`));
+  app.listen(port, () => console.log(`http://localhost:${port}`));
+});
