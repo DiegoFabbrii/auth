7fe32f4a2e26549bff2895e5c35a2aa375943b06
DiegoFabbrii <diegofabbri@outlook.com>
2023-03-03 16:02:24 -0300

    refactor: change mongoose connection key

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const port = process.env.PORT || 3004;
 
 mongoose.set('strictQuery', false);
-mongoose.connect(`${process.env.DB_CONNECTION}`).then(() => {
+mongoose.connect(`mongodb+srv://${process.env.DB_CONNECTION}`).then(() => {
   app.use(express.json());
   app.use(cors());
   app.use(userRoute);
